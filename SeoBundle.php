99de8c10f7ef96f7f7ded5d2f42d7952c72b3349
Falaleev Maxim <max@studio107.ru>
2018-01-03 11:18:43 +0300

    Remove SeoPass

--- SeoBundle.php
+++ SeoBundle.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Mindy Framework.
- * (c) 2017 Maxim Falaleev
+ * (c) 2018 Maxim Falaleev
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -10,14 +12,8 @@
 
 namespace Mindy\Bundle\SeoBundle;
 
-use Mindy\Bundle\SeoBundle\DependencyInjection\Compiler\SeoPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SeoBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        $container->addCompilerPass(new SeoPass());
-    }
 }
